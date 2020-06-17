2ea6c9a30ae9573715b595fb76fc95a1cabf394b
13952522076 <xuma@my.unt.edu>
2020-06-17 00:05:11 -0500

    update

--- metric_learning/sampler.py
+++ metric_learning/sampler.py
@@ -14,7 +14,6 @@ class ClassBalancedBatchSampler(object):
 
     def __iter__(self):
         for _ in range(len(self)):
-            print("iteration")
             yield self.sample_batch()
 
     def _build_reverse_index(self):
@@ -32,8 +31,6 @@ class ClassBalancedBatchSampler(object):
     def sample_batch(self):
         # Real batch size is self.images_per_class * (self.batch_size // self.images_per_class)
         num_classes = self.batch_size // self.images_per_class
-        print("num_classes: {}".format(num_classes))
-        # print("keys: {}".format(self.reverse_index.keys()))
         # sampled_classes = np.random.choice(self.reverse_index.keys(),
         #                                    num_classes,
         #                                    replace=False)
